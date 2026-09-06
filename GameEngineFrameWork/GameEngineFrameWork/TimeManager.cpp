862a3676a45fe40e57c1a370ef15fe472d3deed3
hanabi2356home <hanabira2356@gmail.com>
2026-09-07 03:22:53 +0900

    TimeManager 추가

--- GameEngineFrameWork/GameEngineFrameWork/TimeManager.cpp
+++ GameEngineFrameWork/GameEngineFrameWork/TimeManager.cpp
@@ -1,1 +1,20 @@
 #include "TimeManager.h"
+
+
+TimeManager::TimeManager()
+	:m_deltaTime(0.0f), m_totalTime(0.0f)
+{
+
+}
+TimeManager::~TimeManager()
+{
+
+}
+void TimeManager::Initialize()
+{
+
+}
+void TimeManager::Update()
+{
+
+}
--- GameEngineFrameWork/GameEngineFrameWork/TimeManager.h
+++ GameEngineFrameWork/GameEngineFrameWork/TimeManager.h
@@ -1,5 +1,27 @@
 #pragma once
-class TimeManager
+#include<Windows.h>
+#include"Singleton.h"
+
+class TimeManager : public Singleton<TimeManager>
 {
+	friend class Singleton<TimeManager>;
+
+public:
+	void Initialize();
+	void Update();
+
+	float GetDeltaTime() const { return m_deltaTime; }
+	float GetTotalTime() const { return m_totalTime; }
+	float GetFPS()const { return (m_deltaTime > 0.0f) ? (1.0f / m_deltaTime) : 0.0f; }
+private:
+	TimeManager();
+	~TimeManager() override;
+
+
+	LARGE_INTEGER m_frequency;
+	LARGE_INTEGER m_prevCount;
+
+	float m_deltaTime;
+	float m_totalTime;
 };
 
